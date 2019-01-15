1c55f62abe12a42203e9c54d9b38ae61c4b719eb
foxbloodiron <ariakbar6944@gmail.com>
2019-01-15 16:54:02 +0700

    up

--- resources/views/layouts/_sidebar.blade.php
+++ resources/views/layouts/_sidebar.blade.php
@@ -257,3 +257,11 @@
 <div class="sidebar-overlay" id="sidebar-overlay"></div>
 <div class="sidebar-mobile-menu-handle" id="sidebar-mobile-menu-handle"></div>
 <div class="mobile-menu-handle"></div>
+
+@section('extra_script')
+<script type="text/javascript">
+    
+    alert('a');
+
+</script>
+@endsection
